e4dd935b5a55d9eb3913a099273a27dc227f62f5
Davide <davide.marchi@live.com>
2018-03-13 23:20:30 +0100

    Fix

--- src/main/java/it/unibo/soseng/mdm/acme/venue/FindFeasiblePartners.java
+++ src/main/java/it/unibo/soseng/mdm/acme/venue/FindFeasiblePartners.java
@@ -14,7 +14,7 @@ import it.unibo.soseng.mdm.acme.venue.model.PartnerData;
 public class FindFeasiblePartners implements JavaDelegate {
 
 	public void execute(DelegateExecution execution) throws Exception {
-		/* TODO:
+		/* TODO (??):
 		 * 	1) Retrieve the list of partners from DB or something;
 		 *  2) Send partners' addresses to GIS and get the distance between 
 		 *     their address and conference's address;
@@ -23,35 +23,64 @@ public class FindFeasiblePartners implements JavaDelegate {
 		
 		// FIXME: The Partners can't be a package inside this project, It must be
 		//		  an external service called using Soap and reading their XML	
-		// FIXME: I need a PartnerInformation class to create a list, maybe 
-		//		  I can create the class using the XML created by the Partners
 		// FIXME: I'm not sure about the use of GIS in this point, see the BPMN schema
 		// 		  for other details
 			
-		// A sample partner
-		PartnerData partner = new PartnerData();
-		partner.setName("Dreaming solutions");
-		partner.setType("Hotel");
+		// Retrieve the partner list
+		List<PartnerData> partnerList = new ArrayList<>();
+		partnerList = retrievePartnersList();
+		
+		// The invocation serializationDataFormat("application/json") tells the process engine in which format the variable should be serialized
+		ObjectValue typedPartnerList = Variables.objectValue(partnerList).serializationDataFormat("application/json").create();		
+		execution.setVariable("partnerList", typedPartnerList);
+	}
+	
+	/**
+	 * Create a fake list of partners
+	 * @return fake list of partners
+	 */
+	private List<PartnerData> retrievePartnersList() {
+		List<PartnerData> partnerList = new ArrayList<>();
+		PartnerData partner = new PartnerData();		
 		Address address = new Address();
+		List<Address> addresses = new ArrayList<>();
+		
+		// First partner
+		partner.setName("Dreaming solutions");
+		partner.setType("Manor");
 		address.setCity("Ferrara");
 		address.setCountry("Italia");
 		address.setStreet("Via Garibaldi, 111");
 		address.setPostalCode("44122");
-		List<Address> addresses = new ArrayList<>();
 		addresses.add(address);
 		partner.setAddresses(addresses);
+		// Add to the list
+		partnerList.add(partner);
+
+		// Second partner
+		partner.setName("Hotel Carlton");
+		partner.setType("Hotel");
+		address.setCity("Bologna");
+		address.setCountry("Italia");
+		address.setStreet("Via Zamboni, 44");
+		address.setPostalCode("44444");
+		addresses.add(address);
+		partner.setAddresses(addresses);
+		// Add to the list
+		partnerList.add(partner);
 		
-		// A sample list of partner
-		Integer numberOfPartners = 5;
-		List<PartnerData> partnerList = new ArrayList<>();
-		for (int i = 0; i < numberOfPartners; i++) {
-			partnerList.add(partner);
-		}
+		// Third partner
+		partner.setName("Tiffany");
+		partner.setType("Bar");
+		address.setCity("Padova");
+		address.setCountry("Italia");
+		address.setStreet("Via Belle Arti, 4/A");
+		address.setPostalCode("40111");
+		addresses.add(address);
+		partner.setAddresses(addresses);
+		// Add to the list
+		partnerList.add(partner);
 		
-		// The invocation serializationDataFormat("application/json") tells 
-		// the process engine in which format the variable should be serialized
-		ObjectValue typedPartnerList = 
-				Variables.objectValue(partnerList).serializationDataFormat("application/json").create();		
-		execution.setVariable("partnerList", typedPartnerList);
+		return partnerList;
 	}
 }
