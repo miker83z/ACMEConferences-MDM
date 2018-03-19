c4c5696bc021dfb2b361195b10a5e6dbbd1be3e7
Davide <davide.marchi@live.com>
2018-03-19 15:45:48 +0100

    Fixed all the errors

--- src/main/java/it/unibo/soseng/mdm/acme/venue/PresentOffers.java
+++ src/main/java/it/unibo/soseng/mdm/acme/venue/PresentOffers.java
@@ -1,5 +1,7 @@
 package it.unibo.soseng.mdm.acme.venue;
 
+import static org.camunda.spin.Spin.JSON;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +10,8 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.camunda.bpm.engine.variable.Variables;
 import org.camunda.bpm.engine.variable.value.ObjectValue;
+import org.camunda.spin.SpinList;
+import org.camunda.spin.json.SpinJsonNode;
 
 import it.unibo.soseng.mdm.acme.venue.model.PartnerData;
 
@@ -15,6 +19,8 @@ public class PresentOffers implements JavaDelegate {
 
 	@SuppressWarnings("unchecked")
 	public void execute(DelegateExecution execution) throws Exception {		
+		
+		/*
 		// FIXME: c'è un warning fastidioso perchè Camunda non riesce a lavorare con gli ArrayList
 		
 		// Retrieve the partnerList setted before
@@ -41,6 +47,66 @@ public class PresentOffers implements JavaDelegate {
 	    runtimeService.createMessageCorrelation("offers")
 	    .processInstanceBusinessKey("AB-123")
 	    .setVariable("availablePartners", JSONavailablePartner)
-	    .correlate(); 
+	    .correlate();
+		*/
+		
+		// Get the JSON variable from Camunda engine
+		SpinJsonNode jsonNode = (SpinJsonNode) execution.getVariable("partnerList");
+		
+		// Convert the JSON to a list of PartnerData
+		List<PartnerData> partnerList = new ArrayList<>();
+		partnerList = retrievePartnersFromJSON(jsonNode);
+		
+		// Create a list with only the available and contacted partners
+		List<PartnerData> availablePartners = new ArrayList<>();
+		for (PartnerData partnerData : partnerList) {
+			if (partnerData.getAvailable() && partnerData.getContacted()) {
+				availablePartners.add(partnerData);
+			}
+		}
+		
+		// FIXME: questo pezzo c'è anche in FindFeasiblePartners
+		// Create the string with all partner informations
+		String availablePartnersJSON = "[";
+		for (int i = 0; i < availablePartners.size(); i++) {
+			availablePartnersJSON += availablePartners.get(i).toJSON();
+			if (i < availablePartners.size() - 1) {
+				availablePartnersJSON += ", ";
+			}
+		}
+		availablePartnersJSON += "]";
+		// Convert the string in JSON
+		SpinJsonNode availablePartnersJsonNode = JSON(availablePartnersJSON);	
+		
+		// Send the message setting variables
+	    RuntimeService runtimeService = execution.getProcessEngineServices().getRuntimeService();
+	    runtimeService.createMessageCorrelation("offers")
+	    .processInstanceBusinessKey("AB-123")
+	    .setVariable("availablePartners", availablePartnersJsonNode)
+	    .correlate();					
+	}
+	
+	/**
+	 * 
+	 * @param jsonNode
+	 * @return
+	 */
+	private List<PartnerData> retrievePartnersFromJSON(SpinJsonNode jsonNode) {
+		List<PartnerData> partnerList = new ArrayList<>();
+		
+		// Fetch a list of items when your property is an array of data
+		@SuppressWarnings("rawtypes")
+		SpinList partners = jsonNode.elements();
+		for (int i = 0; i < partners.size(); i++) {
+			// Get the i-th element
+			SpinJsonNode partnerJSON = (SpinJsonNode) partners.get(i);
+			// Convert to PartnerData object
+			PartnerData partnerData = new PartnerData();
+			partnerData.setValueFromJSON(partnerJSON);
+			// Add to list
+			partnerList.add(partnerData);
+		}
+
+		return partnerList;
 	}
 }
